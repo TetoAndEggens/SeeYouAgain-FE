9c6a03fec8a3dc955d83ce892e51adbf71949574
박준영 <87603518+TTprotocol@users.noreply.github.com>
2025-10-30 16:58:09 +0900

    Revert "Feature/component"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,5 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const pretendard = localFont({
-    src: [
-        {
-            path: '../fonts/Pretendard-Regular.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../fonts/Pretendard-Medium.woff2',
-            weight: '500',
-            style: 'normal',
-        },
-        {
-            path: '../fonts/Pretendard-SemiBold.woff2',
-            weight: '600',
-            style: 'normal',
-        },
-        {
-            path: '../fonts/Pretendard-Bold.woff2',
-            weight: '700',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-pretendard',
-    display: 'swap',
-});
 
 export const metadata: Metadata = {
     title: 'SeeYouAgain',
@@ -51,8 +23,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="ko" className={pretendard.variable}>
-            <body className={pretendard.className}>{children}</body>
+        <html lang="ko">
+            <body>{children}</body>
         </html>
     );
 }
