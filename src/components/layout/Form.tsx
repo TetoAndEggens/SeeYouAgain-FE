fbf29ac09514c5f70a1ae921c037b21b420214db
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2025-11-14 23:09:56 +0900

    fix: Vercel 배포를 위한 오류사항 수정
    string 타입을 String 으로 오기한 부분 수정
    지워진 컴포넌트를 호출하고 있는 부분 삭제

--- src/components/layout/Form.tsx
+++ src/components/layout/Form.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 
 interface FormProps {
-    title?: String;
+    title?: string;
     className?: string;
 
     elementChildren?: React.ReactNode;
