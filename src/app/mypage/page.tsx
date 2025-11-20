96aff9263ba2c6819b61f2315385245d9e0182e6
YSW2 <jrgyber@gmail.com>
2025-11-20 20:11:20 +0900

    Feat: 마이페이지 UI 구현
    - 사용자 프로필 영역 구현
      - 프로필 이미지, 이름, 이메일 표시
    
    - 메뉴 목록 구현 및 링크 연결
      - 알림 설정 페이지 링크 (/mypage/settings)
      - 내가 작성한 게시글 페이지 링크 (/mypage/posts)
      - 채팅 내역 페이지 링크 (/chat)
      - 로그아웃 메뉴 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,43 @@
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 const MyPage = () => {
-    return <div>마이페이지</div>;
+    return (
+        <div>
+            <div className="flex items-center gap-4 p-6">
+                <div className="bg-gray-20 border- h-16 w-16 rounded-full"></div>
+                <div>
+                    <p className="mb-2 text-[1.25rem] font-bold">김유기</p>
+                    <p>test@email.com</p>
+                </div>
+            </div>
+            <hr></hr>
+            <div className="flex-1">
+                <Link
+                    href={'mypage/settings'}
+                    className="border-gray-10 flex justify-between border p-4"
+                >
+                    <span>알림 설정</span>
+                    <ChevronRight strokeWidth={1} />
+                </Link>
+                <Link
+                    href={'mypage/posts'}
+                    className="border-gray-10 flex justify-between border p-4"
+                >
+                    <span>내가 작성한 게시글</span>
+                    <ChevronRight strokeWidth={1} />
+                </Link>
+                <Link href={'chat'} className="border-gray-10 flex justify-between border p-4">
+                    <span>채팅 내역</span>
+                    <ChevronRight strokeWidth={1} />
+                </Link>
+                <div className="border-gray-10 flex justify-between border p-4">
+                    <span className="text-destructive">로그아웃</span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MyPage;
