4c5a820160912c436281d968fd03e1c8006ae732
YSW2 <jrgyber@gmail.com>
2025-11-24 01:57:09 +0900

    Feat: 지도 페이지에 카카오맵 통합 및 drawer 상호작용 구현
    - react-kakao-maps-sdk 라이브러리 추가
    - 지도 페이지에 카카오맵 표시 기능 구현
    - 지도 클릭 시 선택한 위치의 좌표를 drawer에 표시
    - VaulDrawer 컴포넌트를 부모에서 제어할 수 있도록 수정
      - open, onOpenChange props 추가로 controlled component로 변경
      - 좌표 정보를 받아 표시하는 기능 추가
    - Drawer.Overlay 활성화 및 Drawer.Description 추가로 접근성 개선
    - useKakaoLoader 커스텀 훅 구현

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,11 +1,47 @@
+'use client';
+
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
 import VaulDrawer from '@/components/ui/drawer';
-import React from 'react';
+import { useState } from 'react';
+import useKakaoLoader from '@/hook/useKakaoLoader';
 
 const MapPage = () => {
+    const [loading, error] = useKakaoLoader();
+    const [coord, setCoord] = useState({ lng: 0, lat: 0 });
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    if (loading) {
+        return <div className="flex h-full items-center justify-center">지도 로딩중..</div>;
+    }
+    if (error) {
+        console.log(error);
+        return <div className="flex h-full items-center justify-center">카카오맵 로딩 에러</div>;
+    }
+
     return (
-        <div>
-            지도 메인페이지
-            <VaulDrawer />
+        <div className="relative h-full w-full">
+            {/* <KakaoMap /> */}
+
+            <Map // 지도를 표시할 Container
+                center={{
+                    // 지도의 중심좌표
+                    lat: 33.450701,
+                    lng: 126.570667,
+                }}
+                style={{
+                    // 지도의 크기
+                    width: '100%',
+                    height: '450px',
+                }}
+                level={3} // 지도의 확대 레벨
+                onClick={(_, mouseEvent) => {
+                    const latlng = mouseEvent.latLng;
+                    setCoord({ lng: latlng.getLng(), lat: latlng.getLat() });
+                    setIsDrawerOpen(true);
+                }}
+            />
+            <VaulDrawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen} coord={coord} />
         </div>
     );
 };
