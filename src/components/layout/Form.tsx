9b19dd86241fa927a829b80ef2dcd2ebc65e8587
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2025-11-25 17:38:13 +0900

    feat: 필터 컴포넌트 임시구현, 실종/목격 목록 페이지 작업 중
    하단 네비게이션 바에서 url에 따라 보여주는 기능 구현
    필터 컴포넌트 구현
    From 컴포넌트에 필수 항목에 * 찍기 추가
    실종/목격 목록 페이지 구현 중

--- src/components/layout/Form.tsx
+++ src/components/layout/Form.tsx
@@ -4,15 +4,19 @@ import { cn } from '@/lib/utils';
 interface FormProps {
     title?: string;
     className?: string;
+    importent?: boolean;
 
     children?: React.ReactNode;
     tagChildren?: React.ReactNode;
 }
 
-export function Form({ title, className, children, tagChildren }: FormProps) {
+export function Form({ title, className, importent, children, tagChildren }: FormProps) {
     return (
         <div className={cn('w-full shrink-0 rounded-lg', className)}>
-            <p className="mb-4 text-[1rem] font-bold">{title}</p>
+            <p className="mb-4 text-[1rem] font-bold">
+                {title}{' '}
+                {importent && <span className="align-text-top text-lg text-red-600">*</span>}{' '}
+            </p>
             {children}
             <div className="overflow-hidden whitespace-nowrap">{tagChildren}</div>
         </div>
