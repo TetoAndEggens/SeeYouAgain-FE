efeac02e6900c88da2093f5265899d767dce05ba
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2025-12-15 19:26:46 +0900

    fix: 기존 Detail 로 따로 페이지 생성했던 것 제거하고 원복. flex-shrink-0을 shrink-0으로 수정

--- src/app/missing/page.tsx
+++ src/app/missing/page.tsx
@@ -1,7 +1,19 @@
-import MissingClientPage from './MissingClientPage';
+'use client';
 
-{
-    /* <Link href="/missing/detail/1"> 디테일 이동</Link> */
+import React from 'react';
+import Link from 'next/link';
+import CustomSelect from '@/components/layout/Selector';
+import { MissingLargeCard } from '@/components/features/missing/MissingLargeCard';
+
+interface MissingProps {
+    data?: {
+        cardType: 'missing' | 'sighting';
+        name: string;
+        tags: string[];
+        location: string;
+        date: string;
+        image: string;
+    }[];
 }
 
 export default async function MissingPage() {
@@ -95,5 +107,60 @@ export default async function MissingPage() {
         },
     ];
 
-    return <MissingClientPage data={testData} />;
+    return (
+        <div className="flex w-full flex-col gap-4 p-5">
+            <div className="flex w-full flex-col items-center gap-2 sm:flex-row">
+                <div className="flex w-full justify-start gap-1">
+                    총 <p className="text-[#FFB84D]">{testData?.length}건</p>의 게시글
+                </div>
+                <div className="flex w-full justify-end">
+                    <div className="flex gap-2">
+                        <CustomSelect
+                            options={[
+                                {
+                                    items: [
+                                        {
+                                            value: '실종',
+                                            name: '실종',
+                                        },
+                                        {
+                                            value: '목격',
+                                            name: '목격',
+                                        },
+                                    ],
+                                },
+                            ]}
+                            placeholder="실종"
+                            defaultValue="실종"
+                        />
+                        <CustomSelect
+                            options={[
+                                {
+                                    items: [
+                                        {
+                                            value: '최신순',
+                                            name: '최신순',
+                                        },
+                                        {
+                                            value: '오래된 순',
+                                            name: '오래된 순',
+                                        },
+                                    ],
+                                },
+                            ]}
+                            placeholder="최신순"
+                            defaultValue="최신순"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="">
+                {testData?.map((item, index) => (
+                    <Link key={`missing-${index}`} href={`missing/detail/${index}`}>
+                        <MissingLargeCard {...item} />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
