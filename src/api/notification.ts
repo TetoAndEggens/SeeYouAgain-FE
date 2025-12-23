82e9fc6c1529d8e71337a5b89f6c826135b8eeba
YSW2 <jrgyber@gmail.com>
2025-12-23 23:01:23 +0900

    Feat: 알림 키워드 관리 기능 추가
    - 키워드 CRUD API 함수 구현 (postKeyword, getKeyword, putKeyword, deleteKeyword)
    - 키워드 관련 타입 정의 (KeywordBody, KeywordResponse 등)
    - settings 페이지 코드 포맷팅 개선

---
+++ src/api/notification.ts
@@ -0,0 +1,32 @@
+import axiosInstance from '@/lib/axios';
+import { ServerResponseType } from '@/types/common';
+import {
+    KeywordBody,
+    KeywordResponse,
+    KeywordUpdateBody,
+    KeywordUpdateResponse,
+} from '@/types/notification';
+
+export const postKeyword = async (
+    request: KeywordBody
+): Promise<ServerResponseType<KeywordResponse>> => {
+    const { data } = await axiosInstance.post('notification/keyword', request);
+    return data;
+};
+
+export const getKeyword = async (): Promise<ServerResponseType<KeywordResponse>> => {
+    const { data } = await axiosInstance.get('notification/keyword');
+    return data;
+};
+
+export const putKeyword = async (
+    request: KeywordUpdateBody
+): Promise<ServerResponseType<KeywordUpdateResponse>> => {
+    const { data } = await axiosInstance.put('notification/keyword/updateAll', request);
+    return data;
+};
+
+export const deleteKeyword = async (id: number): Promise<ServerResponseType<null>> => {
+    const { data } = await axiosInstance.delete(`notification/keyword/${id}`);
+    return data;
+};
