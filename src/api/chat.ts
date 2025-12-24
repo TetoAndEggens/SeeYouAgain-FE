4ed99083866388a8a54d7f94a8b0fc143e1ed30c
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2025-12-24 10:54:25 +0900

    feat: 웹소켓 연결 구현

---
+++ src/api/chat.ts
@@ -0,0 +1,4 @@
+import axiosInstance from '@/lib/axios';
+import { ChatRoomParam, MessageParam, ChatRoomData, Message } from '@/types/chat';
+
+export const fetchChatRoomData = async (params: ChatRoomParam) => {};
