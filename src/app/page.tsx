4ec24407c7ce22206849ab4332fa744c84cfaf44
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2025-12-24 11:48:50 +0900

    fix: 실행 에러 임시 조치
    - chat 페이지들의 데이터 부분 수정으로 인한 데이터 없음

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import { useQuery } from '@tanstack/react-query';
 import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
-import { connect, disconnect } from '@/lib/stompClient';
 
 const testMissingSmallData = [
     {
