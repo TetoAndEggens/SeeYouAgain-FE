596ba84d8917e792b5095f908eefdefb88071344
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2026-02-05 17:33:12 +0900

    feat: 채팅 날짜 형식 변경, 채팅 화면을 항상 최하단으로 내리기

--- src/hook/chat/useMessage.ts
+++ src/hook/chat/useMessage.ts
@@ -1,6 +1,8 @@
 import { getMessage } from '@/api/chat';
-import { MessageParam, MessageData, MessageResponse } from '@/types/chat';
+import { format, formatDistanceToNow } from 'date-fns';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { MessageParam, MessageData, MessageResponse, Message } from '@/types/chat';
+import { ko } from 'date-fns/locale';
 
 export const useMessage = (param: MessageParam) => {
     const {
@@ -32,10 +34,31 @@ export const useMessage = (param: MessageParam) => {
         },
 
         select: (data) => {
-            const mergedData = data.pages.flatMap((page) => page.messages.data);
+            const now = Date.now();
+            const mergedData = data.pages.flatMap((page) => {
+                const message = page.messages.data;
+                let temp: Message[] = [];
+
+                temp = message.map((m) => {
+                    const day = new Date(m.createdAt);
+                    const diff = (now - day.getTime()) / 1000;
+                    let time: string;
+
+                    if (diff < 60 * 1) {
+                        time = '방금 전';
+                    } else if (diff < 60 * 60 * 24) {
+                        time = formatDistanceToNow(day, { addSuffix: true, locale: ko });
+                    } else {
+                        time = format(day, 'MM-dd a h:m', { locale: ko });
+                    }
+
+                    return { ...m, createdAt: time };
+                });
+
+                return temp;
+            });
             const last = data.pages[data.pages.length - 1]?.messages;
-            console.log('useMessage select mergedData : ', mergedData);
-            console.log('useMessage select last : ', last);
+
             return {
                 data: mergedData,
                 size: last?.size ?? param.size ?? 20,
