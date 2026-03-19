b5913d16c7c80a2a035e16d2ce35271d973d51e2
YSW2 <jrgyber@gmail.com>
2026-03-19 16:36:30 +0900

    chore: 서버 주소 env 참조로 변경

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:8080/:path*',
+                destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
             },
         ];
     },
